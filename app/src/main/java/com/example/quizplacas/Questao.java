7108ba05b665be679ad517bc67bb95b75ad47e83
Pedro <pedroluchese@gmail.com>
2026-03-18 20:49:10 -0300

    tela inicial com o framelayout e classe questao

---
+++ app/src/main/java/com/example/quizplacas/Questao.java
@@ -0,0 +1,39 @@
+package com.example.quizplacas;
+
+public class Questao {
+    private int Idimagem;
+    private String respostaCorreta;
+    private String respostaErrada1;
+    private String respostaErrada2;
+    private String respostaErrada3;
+
+    public Questao(int idimagem, String respostaCorreta, String respostaErrada1, String respostaErrada2, String respostaErrada3) {
+        Idimagem = idimagem;
+        this.respostaCorreta = respostaCorreta;
+        this.respostaErrada1 = respostaErrada1;
+        this.respostaErrada2 = respostaErrada2;
+        this.respostaErrada3 = respostaErrada3;
+    }
+
+    public int getIdimagem() {
+        return Idimagem;
+    }
+
+    public String getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    public String getRespostaErrada1() {
+        return respostaErrada1;
+    }
+
+    public String getRespostaErrada2() {
+        return respostaErrada2;
+    }
+
+    public String getRespostaErrada3() {
+        return respostaErrada3;
+    }
+
+
+}
--- app/src/main/java/com/example/quizplacas/TelaQuiz.java
+++ app/src/main/java/com/example/quizplacas/TelaQuiz.java
@@ -1,9 +1,6 @@
 package com.example.quizplacas;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnIniciar;
-    Intent it;
+public class TelaQuiz extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_quiz);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnIniciar = findViewById(R.id.btnIniciar);
     }
-public void proximatela(){
-        it = new Intent(getApplicationContext(), TelaQuiz.class);
-}
 }
